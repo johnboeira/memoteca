a2e8b5e94f21270eda73e824f2e84e55478980cf
João <johnboeira@gmail.com>
2024-01-02 13:15:55 -0300

    refactor : Cria interface pensamento e ajusta tipagem

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,18 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'um pensamento',
-      autoria: 'minha',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit amet. Quo placeat doloribus non dolorum repellat At dolorum voluptatum sit odit iure est ullam illo in quia nobis ad nisi consequatur! Aut rerum sunt eum temporibus suscipit sed enim omnis et perferendis sunt sed adipisci veritatis est alias quidem id officiis tempora. Et sunt autem ut nesciunt similique est totam excepturi. Non reiciendis iusto aut dolor omnis sed Quis voluptatum ab accusantium nobis eum dicta iure aut eveniet omnis eum maxime temporibus!',
-      autoria: 'minha',
-      modelo: 'modelo2'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 
